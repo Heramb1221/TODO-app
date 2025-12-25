99cdc95e9d3039ee7519d697ca06d7a4ce5c8ae2
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-12-25 15:53:24 +0530

    Database

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,17 @@
+import { api } from "@/convex/_generated/api";
 import { useTheme } from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
 
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo)
+  const clearAllTodo = useMutation(api.todos.clearAllTodos)
 
   return (
     <View
